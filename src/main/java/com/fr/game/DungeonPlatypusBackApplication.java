ab1e849a5d6e434a426c8b111bf5dae70b0ca6fc
Jules Dupuis <jules.dupuis51@gmail.com>
2020-06-04 10:06:33 +0200

    initial commit ajout springStater

---
+++ src/main/java/com/fr/game/DungeonPlatypusBackApplication.java
@@ -0,0 +1,13 @@
+package com.fr.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonPlatypusBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonPlatypusBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fr/game/DungeonPlatypusBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonPlatypusBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
